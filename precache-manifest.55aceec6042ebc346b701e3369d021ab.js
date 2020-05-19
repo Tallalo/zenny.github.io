1dca080e025aa159b3ca43ddbc09de7330d77b3e
Tamar Menteshashvili <tamarimenteshashvili7@gmail.com>
2020-05-19 20:05:50 +0400

    initial

---
+++ precache-manifest.55aceec6042ebc346b701e3369d021ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fe8e80d6c8e6d2abbb497adbc84cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90e85cb753a257ea865",
+    "url": "/static/css/main.b3cc471a.chunk.css"
+  },
+  {
+    "revision": "75d843252d733464be82",
+    "url": "/static/js/2.a66597bc.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.a66597bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90e85cb753a257ea865",
+    "url": "/static/js/main.427b9d47.chunk.js"
+  },
+  {
+    "revision": "cca6c644f63f4da66a2d",
+    "url": "/static/js/runtime-main.a414622e.js"
+  }
+]);
